ac9b17f6435b95e5e23522e2939af82a8c95cc20
Vishal Chandra <chapavishal3@gmail.com>
2024-10-15 17:28:57 +0000

    add function to toggle debug to true or false

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -29,7 +29,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.path.isfile("env.py"):
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ['8000-vishalchapa-taskmanager-8uz2jk4vqu4.ws.codeinstitute-ide.net', '.herokuapp.com', ".codeinstitute-ide.net"]
 
