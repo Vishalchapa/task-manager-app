c9e0430ee8e719badf406633fd4fd6ec8a64b9d4
Vishal Chandra <chapavishal3@gmail.com>
2024-10-16 15:06:23 +0000

    add views for about and how_to_use templates

--- core/views.py
+++ core/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 class IndexPage(TemplateView):
     template_name = 'core/index.html'
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def how_to_use(request):
+    return render(request, 'core/how_to_use.html')
