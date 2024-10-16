b3cd60378da3a28272fe0fb4706aa90deff156a3
Vishal Chandra <chapavishal3@gmail.com>
2024-10-16 14:42:53 +0000

    include allauth url path and project url.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tasks/', include('tasks.urls')),
+    path('accounts/', include('allauth.urls')),
     path('', include('core.urls')),  # Core app for static pages
 ]
